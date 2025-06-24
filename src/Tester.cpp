a495698af914f976ff3f137e797c70505d094e14
mpalko26 <mpalko26@unm.edu>
2025-06-23 19:28:03 -0600

    tester outline done, tests added for last two functions

--- src/Tester.cpp
+++ src/Tester.cpp
@@ -1,36 +1,87 @@
-#include <cassert>
 #include <iostream>
 #include "Wordle.h"
 
 
+
+using namespace std;
+
+int correctTests = 0;
+int totalTests = 0;
+
+void clearCounts() {
+  correctTests = 0;
+  totalTests = 0;
+}
+
+void countTest(bool correct) {
+  if (correct) correctTests++;
+  totalTests++;
+}
+
+void printResults(string methodName) {
+  string msg = "";
+  if (correctTests < totalTests) msg = "INCORRECT!";
+  cout << "testing " << methodName << ": passes " << correctTests <<
+    " of " << totalTests << " tests " << msg << endl;
+}
+
+void testSelectRandomWord() {}
+
+void testIsKnownWord() {}
+
+
 void testGetGuessResult() {
-    Wordle game({});
+  Wordle wordle({});
+  char X = Wordle::CORRECT;
+  char o = Wordle::PRESENT;
+  char i = Wordle::MISSING; 
+  
+  countTest((std::vector<char>{o,o,i,X,i}) == wordle.getGuessResult("glass", "sassy"));
+  countTest((std::vector<char>{i, i, i, X, X}) == wordle.getGuessResult("those", "geese"));
+  countTest((std::vector<char>{o, o, i, o, X}) == wordle.getGuessResult("dread", "added"));
+  countTest((std::vector<char>{o, o, o, o, o}) == wordle.getGuessResult("trade", "rated"));
+  countTest((std::vector<char>{i, i, i, X, i, i}) == wordle.getGuessResult("potato", "banana"));
+  countTest((std::vector<char>{o, o, i}) == wordle.getGuessResult("car", "arm"));
+  countTest((std::vector<char>{o, o, o, o}) == wordle.getGuessResult("evil", "live"));
+  countTest((std::vector<char>{i, i, i, i}) == wordle.getGuessResult("evil", "doom"));
 
-    auto result = game.getGuessResult("glass", "sassy");
-    auto expected = std::vector<char>{'o','o','.','X','.'};
-    assert(result == expected);
-
-    result = game.getGuessResult("those", "geese");
-    expected = std::vector<char>{'.','.','.','X','X'};
-    assert(result == expected);
-
-    result = game.getGuessResult("dread", "added");
-    expected = std::vector<char>{'o','o','.','o','X'};
-    assert(result == expected);
-
-    std::cout << "All getGuessResult tests passed.\n";
+  countTest((std::vector<char>{X, i}) == wordle.getGuessResult("ah", "aa"));
+  countTest((std::vector<char>{o, o}) == wordle.getGuessResult("ah", "ha"));
+  countTest((std::vector<char>{i, i}) == wordle.getGuessResult("ah", "ox"));
 }
 
 void testIsWinning() {
-    Wordle game({});
-    assert(game.isWinning({'X','X','X'}));
-    assert(!game.isWinning({'X','o','X'}));
-    std::cout << "All isWinning tests passed.\n";
+  Wordle wordle({});
+  char X = Wordle::CORRECT;
+  char o = Wordle::PRESENT;
+  char i = Wordle::MISSING;
+  countTest(wordle.isWinning({X, X}));
+  countTest(wordle.isWinning({X,X,X,X,X,X,X,X}));
+  countTest(wordle.isWinning({X,X,X,X,X}));
+  countTest(!wordle.isWinning({X,o,X,i,X,o,X,i,X}));
+  countTest(!wordle.isWinning({X, o, i}));
+  countTest(!wordle.isWinning({X,X,X,X,X,i,i,i,i,i}));
+  countTest(!wordle.isWinning({o,o,o,o,o}));
+  countTest(!wordle.isWinning({X,X,X,o,X}));
+  countTest(!wordle.isWinning({i,i,i,i,i}));
 }
 
 int main() {
-    testGetGuessResult();
-    testIsWinning();
-    return 0;
+  clearCounts();
+  testSelectRandomWord();
+  printResults("selectRandomWord");
+
+  clearCounts();
+  testIsKnownWord();
+  printResults("isKnownWord");
+
+  clearCounts();
+  testGetGuessResult();
+  printResults("getGuessResult");
+
+  clearCounts();
+  testIsWinning();
+  printResults("isWinning");
+  return 0;
 }
 
