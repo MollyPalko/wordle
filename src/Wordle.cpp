711373c6b38ca15364c41d9457e868a70fa9217a
mpalko26 <mpalko26@unm.edu>
2025-06-23 20:25:30 -0600

    all functions except getGuessResult are implemented

--- src/Wordle.cpp
+++ src/Wordle.cpp
@@ -9,23 +9,32 @@ Wordle::Wordle(const std::vector<std::string>& wordList) : words(wordList){}
 std::string Wordle::selectRandomWord()
 {
   int i = rand() % (words.size());
-  return "";
+  return words[i];
 }
 
 bool Wordle::isKnownWord(std::string guess)
 {
+  for (std::string s : words) {
+    if (guess == s) return true;
+  }
   return false;
 }
 
 std::vector<char> Wordle::getGuessResult(std::string secret, 
     std::string guess)
 {
-  return {};
+  char arr[] = {'X', 'X'};
+  
+  std::vector<char> vector(arr, arr + (sizeof(arr)/sizeof(char)));
+  return vector;
 }
 
 bool Wordle::isWinning(std::vector<char> guessResult)
 {
-  return false;
+  for (char c : guessResult) {
+    if ( c != CORRECT) return false;
+  }
+  return true;
 }
 
 
--- test.sh
+++ test.sh
@@ -3,8 +3,7 @@
 mkdir -p build
 cd build
 cmake -DBUILD_TESTS=ON ..
-make wordle_tests
-./wordle_tests
-
+make test_wordle
+./test_wordle
 
 
